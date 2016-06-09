e32a9c967dd06b49ce81dc78ad038e9c4b89a35e
p942005405 <2000pan2000@163.com>
2016-06-08 19:26:38 -0700

    add 8 files

---
+++ demo1/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("Hello world this cmake demo1!\n");
+	return 0;	
+}
---
+++ demo2/MathFunctions.h
@@ -0,0 +1,6 @@
+#ifndef _MATHFUNCTIONS_H_
+#define _MATHFUNCTIONS_H_
+
+double power(double a,int b);
+
+#endif
